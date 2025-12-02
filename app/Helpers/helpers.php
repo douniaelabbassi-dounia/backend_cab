cef5f39c62130d0391cc6e32c2801e5c7df7eeb1
Dounia <douniaaelabbassi@gmail.com>
2025-12-02 09:36:49 +0100

    Add missing helpers.php to satisfy composer autoload

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -1,21 +1,4 @@
-<?php 
-
-
-function helper_number_formater(int | float $number,$precision = 1){
-
-    if ($number < 1000) {
-        return $number;
-    } elseif ($number < 1000000) {
-        return round($number / 1000, $precision) . 'K';
-    } elseif ($number < 1000000000) {
-        return round($number / 1000000, $precision) . 'M';
-    } elseif ($number < 1000000000000) {
-        return round($number / 1000000000, $precision) . 'B';
-    } else {
-        return round($number / 1000000000000, $precision) . 'T';
-    }
-
-}
-
-
-
+<?php
+// helpers globaux (ex : fonctions utilitaires)
+// Ajoute ici les fonctions que ton projet utilise.
+// Exemple : if (!function_exists('my_helper')) { function my_helper() { return true; } }

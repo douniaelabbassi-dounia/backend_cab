393483dcca5e76aec4063b0b63d4fcdf048b3fc9
Dounia <douniaaelabbassi@gmail.com>
2025-12-02 11:25:20 +0100

    Add minimal app/Helpers/helpers.php to satisfy composer autoload

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -1,7 +1,6 @@
 <?php
 // app/Helpers/helpers.php
-// Fichier helper minimal pour éviter l'erreur post-autoload-dump.
-// Ajoute ici tes fonctions utilitaires si besoin.
+// Fonctions utilitaires globales basiques pour éviter l'erreur post-autoload-dump
 
 if (! function_exists('app_helper_example')) {
     function app_helper_example(): bool
